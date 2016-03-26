4b197e64e095e56b5a8239a585bf314778f3f923
Hiroshi Takemoto <hiroshi.takemoto@outlook.com>
2016-03-26 16:02:12 -0400

    updated spring-boot version to 1.3.3

--- src/main/java/com/htakemoto/Application.java
+++ src/main/java/com/htakemoto/Application.java
@@ -1,13 +1,9 @@
 package com.htakemoto;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan("com.htakemoto")
-@EnableMongoRepositories(basePackages="com.htakemoto.domain")
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 	
     public static void main(String[] args) throws Exception {
